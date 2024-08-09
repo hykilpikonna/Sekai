fb5252da264ba38f27c20f7356988415f049fe59
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-08-10 05:36:52 +0800

    [+] More eval code

--- llm/one_time/eval_start.py
+++ llm/one_time/eval_start.py
@@ -33,18 +33,18 @@ class DS4Models:
     pipe: pipeline
 
     def __init__(self, path: str | Path):
-        config = TurbomindEngineConfig(tp=1, session_len=1024)
+        config = TurbomindEngineConfig(tp=2, session_len=1024)
         self.pipe = pipeline(path, backend_config=config)
 
-    def gen(self, text: str) -> str:
-        return self.pipe(text, gen_config=GenerationConfig(
+    def gen(self, text: list[str]) -> list[str]:
+        return [v.text for v in self.pipe(text, gen_config=GenerationConfig(
             max_new_tokens=64,
             temperature=0.9,
             repetition_penalty=1.2,  # This is extremely important!
             random_seed=time.time_ns() % 2**32,
             top_k=10,
             top_p=0.9
-        )).text
+        ))]
 
 
 if __name__ == '__main__':
@@ -53,22 +53,29 @@ if __name__ == '__main__':
     args = agupa.parse_args()
     mp = Path(args.model)
 
+    src = Path(__file__).parent
+    data = src.parent / 'data'
+
     model = DS4Models(str(mp))
     # Has columns: "speaker" and "target"
-    pairs = pd.read_csv('data/eval/eval_speakers.csv')
+    pairs = pd.read_csv(data / 'eval/eval_speakers.csv')
 
     # Generate prompts
     pairs['prompt'] = pairs.apply(lambda x: build_prompt(x.speaker, f'{x.target}、最近どうですか？'), axis=1)
 
     # Testing: limit to 10 entries first
     # pairs = pairs.sample(10)
+    
+    # Random shuffle
+    pairs = pairs.sample(frac=1).reset_index(drop=True)
 
     # Generate responses
     # pairs['response'] = pairs.prompt.apply(lambda x: model.gen(x))
-    pairs['response'] = pairs.prompt.progress_apply(lambda x: model.gen(x))
+    pairs['response'] = pairs.prompt.progress_apply(lambda x: model.gen([x])[0])
+    # pairs['response'] = model.gen(list(pairs.prompt))
 
-    Path('data/eval/resp').mkdir(parents=True, exist_ok=True)
+    Path(data / 'eval/resp').mkdir(parents=True, exist_ok=True)
 
     # Save
-    pairs.to_csv(f'data/eval/resp/{mp.stem}.csv', index=False)
+    pairs.to_csv(data / f'eval/resp/{mp.stem}-lmd-pr-tp2.csv', index=False)
 

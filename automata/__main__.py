4d0c7448d0ac23dc48e9d8b04ba59d1afb4cefe3
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-10-05 17:53:22 -0400

    [O] Separate action loop from identification loop

--- automata/__main__.py
+++ automata/__main__.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import time
 from pathlib import Path
 
@@ -9,55 +10,53 @@ from adbutils import adb
 from numpy import ndarray
 from scrcpy import LOCK_SCREEN_ORIENTATION_1
 
+from automata.gamer import SekaiGamer
+from automata.util import priority_win
 from .models import SekaiStageContext, SekaiStage, SekaiStageOp
 from .stage import find_stage, load_stages
-from .config import Config, log, config
+from .config import Config, log, config, global_dict
 
 client: scrcpy.Client
 ctx: SekaiStageContext = None
 frame: ndarray = np.zeros((1, 1, 3), np.uint8)
 frame_time = 0
+last_find_stage = 0
+
+ctx_lock = threading.Lock()
 
 
 def _loop():
-    global ctx
+    global ctx, last_find_stage
 
-    # Create the frame context
-    if not ctx:
-        ctx = SekaiStageContext(client, frame, {}, {}, time.time_ns() // 1_000_000, SekaiStageOp("startup", [], []))
-    ctx.next(frame)
+    # Check if last find stage is too recent (< 1s)
+    if ctx.time - last_find_stage < 1000:
+        return
+    last_find_stage = ctx.time
 
-    # If the context operation is not complete, continue the operation
-    if ctx.last_op_done is None:
-        ac = ctx.last_op.actions[ctx.last_op.action_i]
-        # Update the action
-        log.info(f"> Running action {type(ac).__name__} (started: {ac.started})")
-        if not ac.run(ctx):
+    with ctx_lock:
+        if not ctx.last_op_done:
             return
-        # It finished this time, move to the next action
-        ctx.last_op.action_i += 1
-        # Done with all actions in the operation
-        if ctx.last_op.action_i >= len(ctx.last_op.actions):
-            log.info(f"> Operation completed: {ctx.last_op.name}")
-            ctx.last_op_done = ctx.time
-        return
 
-    # The context operation is complete, we need to look for the next stage
-    stage = find_stage(ctx, stages)
-    if not stage:
-        # Check if timeout has been reached
-        if ctx.time - ctx.last_op_done > ctx.last_op.next_stage_timeout * 1000:
-            log.error(f"Timeout and have not found the next stage: {ctx.last_op.next_stage}")
-            # TODO: Handle timeout
-            ctx.last_op_done = ctx.time
-        return
+        # The context operation is complete, we need to look for the next stage
+        stage = find_stage(ctx, stages)
+        if not stage:
+            # Check if timeout has been reached
+            if ctx.time - ctx.last_op_done > ctx.last_op.next_stage_timeout * 1000:
+                log.error(f"Timeout and have not found the next stage: {ctx.last_op.next_stage}")
+                # Click
+                ctx.tap(config.device.screen_size[0] - 100, config.device.screen_size[1] - 100)
+                ctx.tap(config.device.screen_size[0] - 100, config.device.screen_size[1] - 100)
+                ctx.tap(config.device.screen_size[0] - 100, config.device.screen_size[1] - 100)
+                # TODO: Handle timeout
+                ctx.last_op_done = ctx.time
+            return
 
-    # Perform the operation
-    log.info(f"[{ctx.time}] Entered stage {stage.name}")
-    op = stage.operate(ctx)
-    log.info(f"> Performing operation {op.name}")
-    ctx.last_op = op
-    ctx.last_op_done = None
+        # Perform the operation
+        log.info(f"[{ctx.time}] Entered stage {stage.name}")
+        op = stage.operate(ctx)
+        log.info(f"> Performing operation {op.name}")
+        ctx.last_op = op
+        ctx.last_op_done = None
 
 
 def loop():
@@ -65,6 +64,10 @@ def loop():
         time.sleep(0.1)
 
     while True:
+        if global_dict.get('playing'):
+            time.sleep(1)
+            continue
+
         st = time.time_ns()
         # Check frame timeout 10s
         if st - frame_time > 10_000_000_000:
@@ -73,7 +76,7 @@ def loop():
         _loop()
         et = time.time_ns()
         log.debug(f"Loop time: {(et - st) / 1_000_000:.2f}ms")
-        time.sleep(config.frame_delay)
+        time.sleep(1)
 
 
 def on_frame(new_frame: ndarray):
@@ -81,6 +84,12 @@ def on_frame(new_frame: ndarray):
     This is called when a new frame is received from the device
     """
     if new_frame is None:
+        return
+
+    # Gamer mode takes highest priority
+    p: SekaiGamer = global_dict.get('playing')
+    if p:
+        p.on_frame(new_frame)
         return
 
     global ctx, frame_time, frame
@@ -91,12 +100,35 @@ def on_frame(new_frame: ndarray):
         cv2.imshow("Sekai Automata", new_frame)
         cv2.waitKey(1)
 
+    # Check if ctx_lock is locked
+    if ctx_lock.locked():
+        return
+
     frame = new_frame
     frame_time = time.time_ns()
 
+    with ctx_lock:
+        # Create the frame context
+        ctx.next(frame)
+
+        # If the context operation is not complete, continue the operation
+        if ctx.last_op_done is None:
+            ac = ctx.last_op.actions[ctx.last_op.action_i]
+            # Update the action
+            log.info(f"> Running action {type(ac).__name__} (started: {ac.started})")
+            if not ac.run(ctx):
+                return
+            # It finished this time, move to the next action
+            ctx.last_op.action_i += 1
+            # Done with all actions in the operation
+            if ctx.last_op.action_i >= len(ctx.last_op.actions):
+                log.info(f"> Operation completed: {ctx.last_op.name}")
+                ctx.last_op_done = ctx.time
+            return
+
 
 def run():
-    global client
+    global client, ctx
 
     # Find device
     devices = adb.device_list()
@@ -113,7 +145,13 @@ def run():
         max_width=config.device.screen_size[0]
     )
 
-    client.add_listener(scrcpy.EVENT_INIT, lambda: print("Client started"))
+    ctx = SekaiStageContext(client, frame, {}, {}, time.time_ns() // 1_000_000, SekaiStageOp("startup", [], set()))
+
+    def init():
+        print("Client started")
+        priority_win()
+
+    client.add_listener(scrcpy.EVENT_INIT, init)
     client.add_listener(scrcpy.EVENT_FRAME, on_frame)
 
     # Start the client
